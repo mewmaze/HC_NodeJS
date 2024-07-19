e55c2bd34a0eda4e52f05d487d80729b57efb290
mewmaze <mewmaze@naver.com>
2024-07-19 20:15:03 +0900

    코드 구조 정리

--- models/challenge.js
+++ models/challenge.js
@@ -3,16 +3,41 @@ const {Model, DataTypes} = require("sequelize");
 class Challenge extends Model {
     static init(sequelize) {
         return super.init({
-            challenge_id:{type:DataTypes.INTEGER, primaryKey:true, autoIncrement:true},
-            challenge_name:{type:DataTypes.STRING(255), allowNull:false},
-            description:{type:DataTypes.TEXT},
-            target_period:{type:DataTypes.INTEGER},
-            target_days:{type:DataTypes.INTEGER},
-            participant_count:{type:DataTypes.INTEGER},
-            challenge_img:{type:DataTypes.STRING(255)},
-            start_date:{type:DataTypes.DATE, allowNull:false},
-            end_date:{type:DataTypes.DATE},
-            reward:{type:DataTypes.STRING(255),allowNull:true,}
+            challenge_id:{
+                type:DataTypes.INTEGER, 
+                primaryKey:true, 
+                autoIncrement:true
+            },
+            challenge_name:{
+                type:DataTypes.STRING(255), 
+                allowNull:false
+            },
+            description:{
+                type:DataTypes.TEXT
+            },
+            target_period:{
+                type:DataTypes.INTEGER
+            },
+            target_days:{
+                type:DataTypes.INTEGER
+            },
+            participant_count:{
+                type:DataTypes.INTEGER
+            },
+            challenge_img:{
+                type:DataTypes.STRING(255)
+            },
+            start_date:{
+                type:DataTypes.DATE, 
+                allowNull:false
+            },
+            end_date:{
+                type:DataTypes.DATE
+            },
+            reward:{
+                type:DataTypes.STRING(255),
+                allowNull:true,
+            }
         },{sequelize, modelName:"Challenge", tableName:"challenge", paranoid:false, timestamps:false, charset:"utf8mb4", collate:"utf8mb4_general_ci",})
     }
     // static associate(db){
