98875d101cd4121285ada8e7fa691569751b4c13
sumin <suminshin211@gmail.com>
2024-07-22 17:45:48 +0900

    auth.js 수정

--- routes/auth.js
+++ routes/auth.js
@@ -8,11 +8,13 @@ const SECRET_KEY = 'your_secret_key'; // 환경변수를 사용 권장
 
 router.post('/register', async (req, res, next) => {
   try {
-    const { name, email, password, gender, height, weight, age, interest, profile_picture } = req.body;
+    const { name, nickname, email, password, gender, height, weight, age, profile_picture, interest } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10); // 해시된 비밀번호 생성
 
+    // User 테이블에 사용자 생성
     const newUser = await User.create({
       username: name,
+      nickname,
       email,
       password_hash: hashedPassword, // 해시된 비밀번호 저장
       gender,
@@ -23,6 +25,23 @@ router.post('/register', async (req, res, next) => {
       interests: interest,
       created_at: new Date()
     });
+
+    // Profile 테이블에 기본 값과 함께 user_id 삽입
+    await Profile.create({
+      intro: "자기 소개 내용이 없어요",
+      achievement_count: 0, // 기본 프로필 값 설정
+      user_id: newUser.id, // user_id 대신 id로 수정
+    }, { transaction });
+
+    // 트랜잭션 커밋
+    await transaction.commit();
+
+    res.status(201).json({user_id: newUser.id}); // 생성된 사용자 객체, user_id 반환
+  } catch (error) {
+    console.error('Error creating new user:', error);
+    res.status(500).json({ error: '회원가입 실패' });
+  }
+});
 
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
