8f362dca3d4493eea6c37d0633ba6a0f9f18451f
mewmaze <mewmaze@naver.com>
2024-07-22 13:51:31 +0900

    .env 파일 추가

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config(); // .env 파일 로드
+
 const express = require('express');
 const cors = require('cors');
 const path = require("path");
@@ -17,6 +19,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors({
   origin: "http://localhost:3000", // React 개발 서버 주소
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'] // Authorization 헤더 허용
 }));
 
 app.use(express.json());
@@ -51,11 +55,6 @@ const storage = multer.diskStorage({
 // diskStorage -> 매번 지워줘야함.. 번거로움
 // memoryStorage가 좋다고 함
 const upload = multer({ storage: storage });
-
-app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // 정적파일 제공을 위한 미들웨어 등록
-
-app.use('/challenges',challengeRoutes);
-app.use('/participants',participantRoutes);
 
 app.post('/update', upload.single('profile_picture'), async (req, res) => {
   try {
@@ -92,7 +91,7 @@ app.get('/getProfile', async (req, res) => {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
-    await sequelize.sync({ force: true }); // 새로 초기화
-    // await sequelize.sync({ force: false }); // 데이터베이스 내용 유지
+    // await sequelize.sync({ force: true }); // 새로 초기화
+    await sequelize.sync({ force: false }); // 데이터베이스 내용 유지
     console.log('Database synced');
 });
