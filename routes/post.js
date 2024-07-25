9951b3745094707d652c9f552c873f509def3cae
Dongha <132756629+kdh0825kr@users.noreply.github.com>
2024-07-25 17:39:18 +0900

    [#11] post.js 수정 (#12)

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Post = require('../models/post');
-const {Comment} = require('../models');
+const { Comment } = require('../models');
 const authenticateToken = require('../middleware/authMiddleware'); // JWT 인증 미들웨어
 
 const router = express.Router();
@@ -10,6 +10,21 @@ router.get('/', async (req, res) => {
   try {
     const posts = await Post.findAll();
     res.status(200).json(posts);
+  } catch (err) {
+    console.error('게시글을 불러오는 데 실패했습니다:', err);
+    res.status(500).send('게시글을 불러오는 데 실패했습니다.');
+  }
+});
+
+// 특정 게시글 가져오기
+router.get('/:postId', async (req, res) => {
+  const { postId } = req.params;
+  try {
+    const post = await Post.findByPk(postId);
+    if (!post) {
+      return res.status(404).json({ message: '게시글을 찾을 수 없습니다.' });
+    }
+    res.status(200).json(post);
   } catch (err) {
     console.error('게시글을 불러오는 데 실패했습니다:', err);
     res.status(500).send('게시글을 불러오는 데 실패했습니다.');
@@ -27,6 +42,43 @@ router.post('/', authenticateToken, async (req, res) => {
   } catch (err) {
     console.error('게시글 작성에 실패했습니다:', err); 
     res.status(500).send('게시글 작성에 실패했습니다.');
+  }
+});
+
+// 게시글 수정
+router.put('/:postId', authenticateToken, async (req, res) => {
+  const { postId } = req.params;
+  const { title, content } = req.body;
+
+  try {
+    const post = await Post.findByPk(postId);
+    if (!post) {
+      return res.status(404).json({ message: '게시글을 찾을 수 없습니다.' });
+    }
+    post.title = title;
+    post.content = content;
+    await post.save();
+    res.json(post);
+  } catch (err) {
+    console.error('게시글 수정에 실패했습니다:', err);
+    res.status(500).send('게시글 수정에 실패했습니다.');
+  }
+});
+
+// 게시글 삭제
+router.delete('/:postId', authenticateToken, async (req, res) => {
+  const { postId } = req.params;
+
+  try {
+    const post = await Post.findByPk(postId);
+    if (!post) {
+      return res.status(404).json({ message: '게시글을 찾을 수 없습니다.' });
+    }
+    await post.destroy();
+    res.json({ message: '게시글이 삭제되었습니다.' });
+  } catch (err) {
+    console.error('게시글 삭제에 실패했습니다:', err);
+    res.status(500).send('게시글 삭제에 실패했습니다.');
   }
 });
 
