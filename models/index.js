4cb3119e955bec38639895566be534f1b49a6744
mewmaze <mewmaze@naver.com>
2025-04-02 18:02:34 +0900

    fix: 모델 정의 순서 변경

--- models/index.js
+++ models/index.js
@@ -1,18 +1,23 @@
-const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')[env];
-const User = require('./user');
-const Profile = require('./profile');
-const Challenge = require('./challenge');
-const Participant = require('./participant');
-const ChallengeRecord = require('./challengeRecord');
+const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config")[env];
+const User = require("./user");
+const Profile = require("./profile");
+const Challenge = require("./challenge");
+const Participant = require("./participant");
+const ChallengeRecord = require("./challengeRecord");
 const Post = require("./post");
 const Comment = require("./comment");
 
-let sequelize = new Sequelize(config.database, config.username, config.password, {
+let sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
     ...config,
-    timezone: '+00:00' // UTC 시간대 설정
-});
+    timezone: "+00:00", // UTC 시간대 설정
+  }
+);
 
 const db = {};
 db.sequelize = sequelize;
@@ -26,19 +31,19 @@ db.Post = Post;
 db.Comment = Comment;
 
 User.initiate(sequelize);
-Profile.init(sequelize);
-Challenge.init(sequelize); //init을 권장한다고 해서 init로 사용함
+Challenge.init(sequelize);
 Participant.init(sequelize);
-ChallengeRecord.init(sequelize);
+Profile.init(sequelize);
 Post.initiate(sequelize);
 Comment.initiate(sequelize);
+ChallengeRecord.init(sequelize);
 
 User.associate(db);
-Profile.associate(db);
-//Challenge.associate(db);
+Challenge.associate?.(db);
 Participant.associate(db);
-ChallengeRecord.associate(db);
+Profile.associate(db);
 Post.associate(db);
 Comment.associate(db);
+ChallengeRecord.associate(db);
 
 module.exports = db;
