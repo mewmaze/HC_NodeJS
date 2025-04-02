95e1ea85f53475e397d13b3194fee4121059ba93
mewmaze <mewmaze@naver.com>
2025-04-02 17:37:16 +0900

    fix: User테이블 이름을 User로 만들어지게 수정

--- models/user.js
+++ models/user.js
@@ -1,73 +1,103 @@
-
-const {Model, DataTypes, Sequelize} = require("sequelize");
+const { Model, DataTypes, Sequelize } = require("sequelize");
 
 class User extends Model {
-    static initiate(sequelize) {
-        return User.init({
-            user_id:{ 
-                type:DataTypes.INTEGER, 
-                autoIncrement:true, 
-                primaryKey:true}, //기본키로 설정
-            username:{
-                type:DataTypes.STRING(50), 
-                allowNull:false},
-            nickname:{
-                type:DataTypes.STRING(50), 
-                allowNull:true,
-                defaultValue: ""},
-            email:{
-                type:DataTypes.STRING(100), 
-                unique: true, 
-                allowNull:false},
-            password_hash:{
-                type:DataTypes.STRING(255), 
-                allowNull:false},
-            gender:{
-                type:DataTypes.ENUM('남성', '여성'),
-                allowNull: true,
-                defaultValue: '남성'},
-            height:{
-                type:DataTypes.FLOAT, 
-                allowNull:false},
-            weight:{
-                type:DataTypes.FLOAT, 
-                allowNull:false},
-            bmi:{
-                type:DataTypes.FLOAT, 
-                allowNull:true},
-            age:{
-                type:DataTypes.TEXT, 
-                allowNull:true},
-            profile_picture:{
-                type:DataTypes.STRING(255), // 수정: 문자열 타입으로 변경
-                allowNull:true,
-                defaultValue: ''}, // 필요에 따라 allowNull 설정 변경
-            goal_height:{                 // 예네 cmd에서 null로 뜸
-                type:DataTypes.FLOAT, 
-                allowNull:true},
-            goal_weight:{                 // 예네 cmd에서 null로 뜸
-                type:DataTypes.FLOAT, 
-                allowNull:true},
-            goal_bmi:{                 // 예네 cmd에서 null로 뜸
-                type:DataTypes.FLOAT,
-                allowNull:true},            
-            interests:{
-                type:DataTypes.ENUM('런닝', '헬스', '자전거', '다이어트'),
-                allowNull: false,
-                defaultValue: '런닝'},
-            current_challenge_id:{  // 예도 cmd에서 null로 뜸
-                type:DataTypes.INTEGER, 
-                allowNull:true},
-            created_at:{
-                type:DataTypes.DATE,
-                defaultValue:Sequelize.fn('now'),
-                allowNull:false},
-        },{sequelize, modelName:"User", tableName:"user", paranoid:false, timestamps:false})
-    }
-    static associate(db){
-        // User.hasMany(db.Order, {foreignKey:"bookid"}) // 설정 필요
-        User.hasMany(db.Challenge, {foreignKey: "user_id"})
-        this.hasMany(db.Post, { foreignKey: "user_id", as: 'posts' });
-    }
+  static initiate(sequelize) {
+    return User.init(
+      {
+        user_id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+        }, //기본키로 설정
+        username: {
+          type: DataTypes.STRING(50),
+          allowNull: false,
+        },
+        nickname: {
+          type: DataTypes.STRING(50),
+          allowNull: true,
+          defaultValue: "",
+        },
+        email: {
+          type: DataTypes.STRING(100),
+          unique: true,
+          allowNull: false,
+        },
+        password_hash: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+        },
+        gender: {
+          type: DataTypes.ENUM("남성", "여성"),
+          allowNull: true,
+          defaultValue: "남성",
+        },
+        height: {
+          type: DataTypes.FLOAT,
+          allowNull: false,
+        },
+        weight: {
+          type: DataTypes.FLOAT,
+          allowNull: false,
+        },
+        bmi: {
+          type: DataTypes.FLOAT,
+          allowNull: true,
+        },
+        age: {
+          type: DataTypes.TEXT,
+          allowNull: true,
+        },
+        profile_picture: {
+          type: DataTypes.STRING(255), // 수정: 문자열 타입으로 변경
+          allowNull: true,
+          defaultValue: "",
+        }, // 필요에 따라 allowNull 설정 변경
+        goal_height: {
+          // 예네 cmd에서 null로 뜸
+          type: DataTypes.FLOAT,
+          allowNull: true,
+        },
+        goal_weight: {
+          // 예네 cmd에서 null로 뜸
+          type: DataTypes.FLOAT,
+          allowNull: true,
+        },
+        goal_bmi: {
+          // 예네 cmd에서 null로 뜸
+          type: DataTypes.FLOAT,
+          allowNull: true,
+        },
+        interests: {
+          type: DataTypes.ENUM("런닝", "헬스", "자전거", "다이어트"),
+          allowNull: false,
+          defaultValue: "런닝",
+        },
+        current_challenge_id: {
+          // 예도 cmd에서 null로 뜸
+          type: DataTypes.INTEGER,
+          allowNull: true,
+        },
+        created_at: {
+          type: DataTypes.DATE,
+          defaultValue: Sequelize.fn("now"),
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        modelName: "User",
+        freezeTableName: true,
+        tableName: "User",
+        paranoid: false,
+        timestamps: false,
+      }
+    );
+  }
+  static associate(db) {
+    // User.hasMany(db.Order, {foreignKey:"bookid"}) // 설정 필요
+    User.hasMany(db.Challenge, { foreignKey: "user_id" });
+    this.hasMany(db.Post, { foreignKey: "user_id", as: "posts" });
+  }
 }
 module.exports = User;
