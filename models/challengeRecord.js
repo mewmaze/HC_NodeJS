39c107c5f9b3100e3e5643a3c9af4547f2d80f9a
mewmaze <mewmaze@naver.com>
2025-04-02 17:54:39 +0900

    fix :sequelize foreign key 모델명 소문자로 변경

--- models/challengeRecord.js
+++ models/challengeRecord.js
@@ -1,39 +1,51 @@
-const {Model, DataTypes} = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
 
 class ChallengeRecord extends Model {
-    static init(sequelize) {
-        return super.init({
-            record_id:{
-                type:DataTypes.INTEGER, 
-                primaryKey:true, 
-                autoIncrement:true,
-                allowNull:false
-            },
-            participant_id:{
-                type:DataTypes.INTEGER, 
-                allowNull:false,
-                references: {
-                    model: "Participant",
-                    key: "participant_id",
-                }
-            },
-            challenge_id: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                references: {
-                    model: "Challenge", // 참조할 모델 이름 (Challenge 테이블)
-                    key: "challenge_id", // 참조할 모델의 기본 키 (challenge_id)
-                },
-            },
-            completion_date:{ //챌린지를 수행한 날짜
-                type:DataTypes.DATE, 
-                allowNull:false
-            },
-        },{sequelize, modelName:"ChallengeRecord", tableName:"challengeRecord", paranoid:false, timestamps:false, charset:"utf8mb4", collate:"utf8mb4_general_ci",})
-    }
-    static associate(models){
-        this.belongsTo(models.Participant, {foreignKey:"participant_id"});
-        this.belongsTo(models.Challenge, {foreignKey:"challenge_id"});
-    }
+  static init(sequelize) {
+    return super.init(
+      {
+        record_id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false,
+        },
+        participant_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: "participant",
+            key: "participant_id",
+          },
+        },
+        challenge_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: "challenge", // 참조할 모델 이름 (Challenge 테이블)
+            key: "challenge_id", // 참조할 모델의 기본 키 (challenge_id)
+          },
+        },
+        completion_date: {
+          //챌린지를 수행한 날짜
+          type: DataTypes.DATE,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        modelName: "ChallengeRecord",
+        tableName: "challengeRecord",
+        paranoid: false,
+        timestamps: false,
+        charset: "utf8mb4",
+        collate: "utf8mb4_general_ci",
+      }
+    );
+  }
+  static associate(models) {
+    this.belongsTo(models.Participant, { foreignKey: "participant_id" });
+    this.belongsTo(models.Challenge, { foreignKey: "challenge_id" });
+  }
 }
 module.exports = ChallengeRecord;
